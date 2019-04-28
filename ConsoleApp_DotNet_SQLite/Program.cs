c74de1bb056ea00580ee1f2e29289f0bbb763a9b
Ro <rodrigoverasantis@gmail.com>
2019-04-28 10:19:05 -0400

    Se incluyen servicios para consultar la base de datos.

--- ConsoleApp_DotNet_SQLite/Program.cs
+++ ConsoleApp_DotNet_SQLite/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ConsoleApp_DotNet_SQLite.Domain;
+using ConsoleApp_DotNet_SQLite.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +9,10 @@ using System.Threading.Tasks;
 namespace ConsoleApp_DotNet_SQLite {
   class Program {
     static void Main(string[] args) {
+      var personaService = new PersonaService();
+      //var check = personaService.Agregar(new Persona { Nombre = "Jaun",ApellidoPaterno = "Perez",ApellidoMaterno = "",FechaNacimiento = DateTime.Now });
+      var personas = personaService.ObtenerTodo();
+      Console.WriteLine(personas.Count());
     }
   }
 }
--- ConsoleApp_DotNet_SQLite/Services/DatabaseService.cs
+++ ConsoleApp_DotNet_SQLite/Services/DatabaseService.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿using ConsoleApp_DotNet_SQLite.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp_DotNet_SQLite.Services {
-  public abstract class DatabaseService<T>:IDatabaseService<T> {
+  public abstract class DatabaseService<T>:IDatabaseService<T> where T : class {
     public bool Actualizar(T _entidad) {
       throw new NotImplementedException();
     }
 
     public bool Agregar(T _entidad) {
-      throw new NotImplementedException();
+      try {
+        using (var context = new DataContext()) {
+          context.Set<T>().Add(_entidad);
+          var cambios = context.SaveChanges();
+          return cambios > 0;
+        }
+      } catch (Exception e) {
+        throw new Exception($"Error al intentar agregar una entidad de tipo {typeof(T)}",e);
+      }
     }
 
     public bool Eliminar(T _entidad) {
@@ -23,7 +32,14 @@ namespace ConsoleApp_DotNet_SQLite.Services {
     }
 
     public IEnumerable<T> ObtenerTodo() {
-      throw new NotImplementedException();
+      try {
+        using (var context = new DataContext()) {
+          var entidades = context.Set<T>().ToList();
+          return entidades;
+        }
+      } catch (Exception e) {
+        throw new Exception($"Error al intentar obtener todas las entidadesde tipo {typeof(T)}",e);
+      }
     }
   }
 }
